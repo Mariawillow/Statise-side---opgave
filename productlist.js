5651a7f82f080d29df80fd69720238e182bcfcb7
Mariawillow <mani0013@stud.kea.dk>
2024-09-02 14:19:00 +0200

    j

--- productlist.js
+++ productlist.js
@@ -1,29 +1,35 @@
-fetch("https://kea-alt-del.dk/t7/api/products")
-.then((res) => res.json())
-.then(showProducts);
+window.addEventListener("DOMContentLoaded", init);
 
+const productsURI = "https://kea-alt-del.dk/t7/api/products";
 
-function showProducts(products){
-    products.forEach(showProduct);
+let productList;
+let productTemplate;
 
+function init() {
+  productList = document.querySelector("#product_list");
+  console.log("productList", productList);
+
+  productTemplate = document.querySelector("template").content;
+  console.log("productTemplate", productTemplate);
+
+  fetch(productsURI)
+    .then((response) => {
+      console.log("response", response);
+      return response.json();
+    })
+    .then(handleData);
 }
 
-function showProduct(product){
-
-    console.log(product);
-    // Fange template 
-    const template = document.querySelector("#productTemplate").content;
-    // Lave kopi
-const copy = template.cloneNode(true);
-    // ændre indhold
-copy.querySelector("h3").textContent=product.productdisplayname;
-if(product.soldout){
-    // productet er udsolgt
-    copy.querySelector("article").classList.add("soldOut");
+function handleData(json) {
+  console.log("json", json);
+  json.forEach(showProduct);
 }
 
-    // appende
-    document.querySelector("main").appendChild(copy);
+function showProduct(product) {
+  const clone = productTemplate.cloneNode(true);
+  clone.querySelector("h3").textContent = product.productdisplayname;
+  clone.querySelector("p").textContent = product.season;
+  clone.querySelector("img").src =`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  clone.querySelector("img").alt = product.productdisplayname;
+  productList.appendChild(clone);
 }
-
-// 
